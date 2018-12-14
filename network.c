c8eeda4882398ed89250fe0592793819cbb05aa2
root <root@localhost.localdomain>
2018-12-14 22:19:41 +0800

    some code comments

--- network.c
+++ network.c
@@ -23,15 +23,13 @@
 #include <crypt.h>
 #include <pwd.h>
 
-char *full_encrypted_pass;
-char *salt;
-char *id;
-
-struct passwd *pw;
-struct spwd *sp;
+struct passwd *pw; // struct for password file
+struct spwd *sp;  // struct for shadow password file
 
 struct addrinfo min_info_strc; // addrinfo with minimum of information
 struct addrinfo *full_info_strc; // addrinfo which will be filled by getaddrinfo()
+
+char *full_encrypted_pass;
 
 int sck, new_sck; // my and client socket's address
 
@@ -99,6 +97,8 @@ void e_accept(int socket_address)
 
   while(1)
   {
+    full_encrypted_pass = malloc(150);
+
     addr_size = sizeof their_addr; // initializing their address's size
 
     new_sck = accept(socket_address, (struct sockaddr *)&their_addr, &addr_size); // my socket's adress, their sockaddr, their address's size
@@ -112,29 +112,29 @@ void e_accept(int socket_address)
     char recieve_login_buffer[20]; // buffer for recieved login
     char recieve_password_buffer[20]; // buffer for recieved password
 
-    char *sending_msg = malloc(100);
+    char *sending_msg = malloc(100); // pointer for every message that server sends to client
 
     freeaddrinfo(full_info_strc); // clear the structure
 
-    int password_and_login_are_correct = 0;
-    while (password_and_login_are_correct == 0)
+    int password_and_login_are_correct = 0; // (instead of bool)
+    while (password_and_login_are_correct == 0) // checking if entered login and password are not correct
     {
       int login_bytes_recieved, password_bytes_recieved; // variables for counts of recieved bytes
 
-      sending_msg = "proftpd clone 0.1beta\n";
+      sending_msg = "proftpd clone 0.1beta\n"; // puts a message to my variable
+      send(new_sck, sending_msg, strlen(sending_msg), 0); // send() this message
+      memset(&sending_msg, 0, sizeof sending_msg); // clear my favourite variable
+
+      sending_msg = "Login: \n"; // same thing
       send(new_sck, sending_msg, strlen(sending_msg), 0);
       memset(&sending_msg, 0, sizeof sending_msg);
 
-      sending_msg = "Login: \n";
-      send(new_sck, sending_msg, strlen(sending_msg), 0);
-      memset(&sending_msg, 0, sizeof sending_msg);
-
-      login_bytes_recieved = recv(new_sck, &recieve_login_buffer, sizeof recieve_login_buffer, 0);
-      if (login_bytes_recieved > 2)
+      login_bytes_recieved = recv(new_sck, &recieve_login_buffer, sizeof recieve_login_buffer, 0); // recv() the login from client to first buffer
+      if (login_bytes_recieved > 2) // if length of login is bigger than 0 (recv() things that recieves 2 bytes more data)
       {
-        recieve_login_buffer[login_bytes_recieved - 2] = '\0';
-        printf("Login entered: %s\n", recieve_login_buffer);
-        printf("recieved %d bytes\n", login_bytes_recieved);
+        recieve_login_buffer[login_bytes_recieved - 2] = '\0'; // add the null-terminator to avoid segmentation fault
+        printf("Login entered: %s\n", recieve_login_buffer);  // printf() the sended login
+        printf("recieved %d bytes\n", login_bytes_recieved);  // and number of bytes
       }
       else
       {
@@ -142,14 +142,14 @@ void e_accept(int socket_address)
         exit(1);
       }
 
-      sending_msg = "Password: \n";
+      sending_msg = "Password: \n"; // same thing [2]
       send(new_sck, sending_msg, strlen(sending_msg), 0);
       memset(&sending_msg, 0, sizeof sending_msg);
 
-      password_bytes_recieved = recv(new_sck, &recieve_password_buffer, sizeof recieve_password_buffer, 0);
-      if (password_bytes_recieved > 2)
+      password_bytes_recieved = recv(new_sck, &recieve_password_buffer, sizeof recieve_password_buffer, 0);// recv() the password from client to second buffer
+      if (password_bytes_recieved > 2) // if it longer than 0
       {
-        recieve_password_buffer[password_bytes_recieved - 2] = '\0';
+        recieve_password_buffer[password_bytes_recieved - 2] = '\0'; // null-terminator
         printf("Password entered: %s\n", recieve_password_buffer);
         printf("recieved %d bytes\n", password_bytes_recieved);
       }
@@ -159,19 +159,19 @@ void e_accept(int socket_address)
         exit(1);
       }
 
-      sp = getspnam(recieve_login_buffer);
-      full_encrypted_pass = sp->sp_pwdp;
-      printf("%s\n", full_encrypted_pass);
+      sp = getspnam(recieve_login_buffer);  // find the line in shadow file by login
+      full_encrypted_pass = sp->sp_pwdp;  // put encrypted password ($id$salt$hash) from that line on variable
+      printf("%s\n", full_encrypted_pass); // printf it
 
-      // осталось извлечь соль, хэш и id, зашифровать присланный пароль с использованием соли, сравнить полученное значение с хэшем
-
-      if (strcmp(crypt(recieve_password_buffer, full_encrypted_pass), full_encrypted_pass) == 0)
+      if (strcmp(crypt(recieve_password_buffer, full_encrypted_pass), full_encrypted_pass) == 0) // if encrypted recieved password with salt equals salt
       {
-        password_and_login_are_correct = 1;
+        password_and_login_are_correct = 1; // "boolean" variable for checking recieves value "true"
       }
 
-      memset(&recieve_login_buffer, 0, sizeof recieve_login_buffer); // check if these buffers are empty
-      memset(&recieve_password_buffer, 0, sizeof recieve_password_buffer);
+      memset(&recieve_login_buffer, 0, sizeof recieve_login_buffer); // free the buffer for login...
+      memset(&recieve_password_buffer, 0, sizeof recieve_password_buffer); // ...and for password...
+
+      memset(&full_encrypted_pass, 0, sizeof full_encrypted_pass); // ...and also for encrypted password
     }
   }
 }
@@ -179,7 +179,6 @@ void e_accept(int socket_address)
 // entry point of programm
 int main(int argc, char *argv[])
 {
-  full_encrypted_pass = malloc(150);
   e_gai(argv[1]);
   e_socket(full_info_strc->ai_family, full_info_strc->ai_socktype, full_info_strc->ai_protocol);
   e_bind(sck, full_info_strc->ai_addr, full_info_strc->ai_addrlen);
